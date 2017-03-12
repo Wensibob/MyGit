dee5b4b83726e63e36c01c2a78a880a9c961775a
BOB <wensibob@gmail.com>
2017-03-12 10:13:41 +0800

    add feature-2 on feature-2

--- app/src/main/java/com/bob/mygit/MainActivity.java
+++ app/src/main/java/com/bob/mygit/MainActivity.java
@@ -8,11 +8,13 @@ import android.widget.Button;
 public class MainActivity extends AppCompatActivity {
 
     private Button btn_feature_1;
+    private Button btn_feature_2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         btn_feature_1 = (Button) findViewById(R.id.btn_feature_1);
+        btn_feature_2 = (Button) findViewById(R.id.btn_feature_2);
     }
 
     /**
@@ -27,4 +29,18 @@ public class MainActivity extends AppCompatActivity {
             btn_feature_1.setText(R.string.feature_1);
         }
     }
+
+    /**
+     * feature-2
+     * display HelloAndroid
+     * @param view
+     */
+    public void feature2(View view) {
+        if (btn_feature_2.getText().toString().equals("功能2")) {
+            btn_feature_2.setText(R.string.feature_2_dis);
+        } else {
+            btn_feature_2.setText(R.string.feature_2);
+        }
+    }
+
 }
