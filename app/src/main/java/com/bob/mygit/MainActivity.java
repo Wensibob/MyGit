3b5a926041619019861f0119367f25e385e7844f
BOB <wensibob@gmail.com>
2017-03-12 10:05:59 +0800

    init My-git repo

---
+++ app/src/main/java/com/bob/mygit/MainActivity.java
@@ -0,0 +1,13 @@
+package com.bob.mygit;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
